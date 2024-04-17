4421d2232657ed55a0c5e8de3164ac0fd7836d45
Renitha <renithasarima@gmail.com>
2024-04-17 20:50:28 +0700

    add menambah catatan

--- src/Catatan.kt
+++ src/Catatan.kt
@@ -1,5 +1,12 @@
 class Catatan(val judul: String, val isi: String)
 
+class PengelolaCatatan {
+    private val daftarCatatan = mutableListOf<Catatan>()
+    
+    fun tambahCatatan(catatan: Catatan) {
+        daftarCatatan.add(catatan)
+    }
+
 fun main() {
     // Debugging punya Haidar, boleh dihapus
     /*
