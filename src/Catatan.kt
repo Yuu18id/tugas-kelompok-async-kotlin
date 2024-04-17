05f53d9a7a4080f2110905c69866fc3bb1cab672
Dev-Hisaki <muharuichijou@gmail.com>
2024-04-17 20:02:22 +0700

    add : Class Catatan with constructor judul dan isi

---
+++ src/Catatan.kt
@@ -0,0 +1,12 @@
+class Catatan(val judul: String, val isi: String)
+
+fun main() {
+    // Debugging punya Haidar, boleh dihapus
+    /*
+    val note = Catatan("Judul Catatan", "Isi Catatan")
+    fun tampilkanCatatan (catatan: Catatan): String {
+        return "${catatan.judul}"
+    }
+    println(tampilkanCatatan(note))
+    */
+}
